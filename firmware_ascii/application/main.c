1f1b5b6855e6d09b9215347b7f7a9ac04f2248c8
wangtao96 <2031587450@qq.com>
2017-07-05 13:12:36 +0800

    ne'w

--- firmware_ascii/application/main.c
+++ firmware_ascii/application/main.c
@@ -14,6 +14,7 @@ All Global variable names shall start with "G_"
 /* New variables */
 volatile u32 G_u32SystemFlags = 0;                     /* Global system flags */
 volatile u32 G_u32ApplicationFlags = 0;                /* Global applications flags: set when application is successfully initialized */
+u16 u16testnumber;
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 /* External global variables defined in other files (must indicate which file they are defined in) */
@@ -42,6 +43,9 @@ the 1ms period.
 void main(void)
 {
   G_u32SystemFlags |= _SYSTEM_INITIALIZING;
+  u32 u32show;
+  u16testnumber=1996;
+  
 
   /* Low level initialization */
   WatchDogSetup(); /* During development, does not reset processor if timeout */
@@ -49,12 +53,15 @@ void main(void)
   ClockSetup();
   InterruptSetup();
   SysTickSetup();
+  u32show=0;
+  u16testnumber-=2;
 
   /* Driver initialization */
   MessagingInitialize();
   UartInitialize();
   DebugInitialize();
-
+  
+  
   /* Debug messages through DebugPrintf() are available from here */
 
   ButtonInitialize();
@@ -84,6 +91,8 @@ void main(void)
   while(1)
   {
     WATCHDOG_BONE();
+    u32show++;
+    u16testnumber--;
     
     /* Drivers */
     LedUpdate();
